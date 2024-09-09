5c65938604ff9125873368b5d7dabd9d44fdf889
the-first-elder <emmanuelmayowa95@gmail.com>
2024-09-09 13:44:14 +0100

    sample algo befre swap

--- src/RouterHook.sol
+++ src/RouterHook.sol
@@ -9,12 +9,12 @@ import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {BalanceDeltaLibrary, BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
-import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {TickMath} from "v4-core/libraries/TickMath.sol";
-import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
 import {StateLibrary} from "v4-core/libraries/StateLibrary.sol";
 import {FixedPointMathLib} from "solmate/src/utils/FixedPointMathLib.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {IQuoter} from "v4-periphery/src/interfaces/IQuoter.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
 
 contract RouterHook is BaseHook {
     using StateLibrary for IPoolManager;
@@ -23,9 +23,12 @@ contract RouterHook is BaseHook {
     using CurrencyLibrary for Currency;
     using BalanceDeltaLibrary for BalanceDelta;
 
+    IQuoter public quoter;
+
     constructor(IPoolManager _manager) BaseHook(_manager) {}
 
     mapping(address token0 => mapping(address token1 => address rebalancerAddress)) public deployedRebalancerAddress;
+    int256 LARGE_SWAP_THRESHOLD = 1 ether;
 
     function rebalancerFactory(address _token0, address _token1) public {
         address rebalance = deployedRebalancerAddress[_token0][_token1];
@@ -33,6 +36,51 @@ contract RouterHook is BaseHook {
 
         JITRebalancer jitRebalancer = new JITRebalancer(_token0, _token1, address(this));
         deployedRebalancerAddress[_token0][_token1] = address(jitRebalancer);
+    }
+
+    function beforeSwap(
+        address,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata swapParams,
+        bytes calldata hookData
+    ) external override returns (bytes4, BeforeSwapDelta, uint24) {
+        address jit = getFactoryAddress(Currency.unwrap(key.currency0), Currency.unwrap(key.currency1));
+        require(jit != address(0), "pool doesnt exist for pair");
+
+        // (uint160 sqrtPriceX96, int24 currentTick,,) = poolManager.getSlot0(key.toId());
+        uint256 amountIn = uint256(swapParams.amountSpecified);
+        bool zeroForOne = swapParams.zeroForOne;
+        uint160 MAX_SLIPPAGE = zeroForOne ? TickMath.MIN_SQRT_PRICE + 1 : TickMath.MAX_SQRT_PRICE - 1;
+
+        // precalculate price ....
+        (int128[] memory deltaAmounts, uint160 sqrtPriceX96After,) = quoter.quoteExactInputSingle(
+            IQuoter.QuoteExactSingleParams(key, zeroForOne, uint128(amountIn), MAX_SLIPPAGE, hookData)
+        );
+
+        // Interpret the delta amounts
+        int128 deltaSpecified = deltaAmounts[0]; // Change in the amount of the specified token
+        int128 deltaUnspecified = deltaAmounts[1]; // Change in the amount of the unspecified token
+
+        // Create BeforeSwapDelta
+        BeforeSwapDelta delta = toBeforeSwapDelta(deltaSpecified, deltaUnspecified);
+
+        // perform JIT
+        if (swapParams.amountSpecified > LARGE_SWAP_THRESHOLD) {
+            // Calculate the new tick after the swap
+            int24 newTick = TickMath.getTickAtSqrtPrice(sqrtPriceX96After);
+            // uint256 liquidity = poolManager.
+        }
+        // get jit address to get liquidity from ...
+        // get amount to be swapped if large enough then
+        // get price tick the large swap would occur at
+        // add liquidity to the price tick
+        // revert HookNotImplemented();getSlot0
+        return (this.beforeSwap.selector, delta, 0);
+    }
+
+    function getFactoryAddress(address _token0, address _token1) public view returns (address) {
+        address rebalance = deployedRebalancerAddress[_token0][_token1];
+        return rebalance;
     }
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
